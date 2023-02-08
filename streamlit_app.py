a69f7cd9e2335308bf6427e2f637f07a7e0f9839
AlbertoEJ <99564621+AlbertoEJ@users.noreply.github.com>
2023-02-08 01:00:18 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,7 @@ def decrypt(key, data):
 
 key=b'12345678901234567890123456789012'
 
-cifrado=encrypt(key,b'Holaaaa')
 
-desencriptado=decrypt(key,cifrado)
 
 st.title('Cifraco de un mensaje con AES')
 st.text('Código realizado en Python por Elena :D')
@@ -28,5 +26,8 @@ st.text('Código realizado en Python por Elena :D')
 texto = st.text_input('Ingresa un texto a cifrar:',)
 mensaje=bytes(texto, 'utf-8')
 
+cifrado=encrypt(key,mensaje)
+
+desencriptado=decrypt(key,cifrado)
 st.write('El texto cifrado es: ', mensaje)
 st.write('El texto descifrado es: ', desencriptado)
