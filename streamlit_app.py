48e5b430b318b6cfc29a694892e57f6eacef47a7
AlbertoEJ <99564621+AlbertoEJ@users.noreply.github.com>
2023-02-07 22:49:51 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,25 @@ if "visibility" not in st.session_state:
 col1, col2 = st.columns(2)
 
 with col1:
+    st.checkbox("Disable text input widget", key="disabled")
+    st.radio(
+        "Set text input label visibility 👉",
+        key="visibility",
+        options=["visible", "hidden", "collapsed"],
+    )
+    st.text_input(
+        "Placeholder for the other text input widget",
+        "This is a placeholder",
+        key="placeholder",
+    )
+
+with col2:
     text_input = st.text_input(
-        "Ingresa un texto 👇",
+        "Enter some text 👇",
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
         placeholder=st.session_state.placeholder,
     )
 
-with col2:
     if text_input:
         st.write("You entered: ", text_input)
