f011273ac13778b69f74fbfd32a1ba971204b8c0
AlbertoEJ <99564621+AlbertoEJ@users.noreply.github.com>
2023-02-07 22:56:54 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-title = st.text_input('Movie title', 'Life of Brian',key=int)
+title = st.text_input('Movie title', 'Life of Brian')
 
-suma=title+1
+suma=str(title+1)
 st.write('La suma es:', suma)
