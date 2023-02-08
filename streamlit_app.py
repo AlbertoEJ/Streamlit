8259c8233a028aa3fc0adc7e1c70b49fd8fa35ea
AlbertoEJ <99564621+AlbertoEJ@users.noreply.github.com>
2023-02-08 00:51:42 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,28 @@
 import streamlit as st
 from Crypto.Cipher import AES
 
+def encrypt(key, data):
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+
+    return cipher.nonce + tag + ciphertext
+
+def decrypt(key, data):
+    nonce = data[:AES.block_size]
+    tag = data[AES.block_size:AES.block_size * 2]
+    ciphertext = data[AES.block_size * 2:]
+
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    
+    return cipher.decrypt_and_verify(ciphertext, tag)
+
+key=b'12345678901234567890123456789012'
+
+cifrado=encrypt(key,b'Holaaaa')
+
 st.title('Tarea de cifrado de Elena Herandez')
 st.text('¡Esto es puro Pythoooon!')
 
 texto = st.text_input('Ingresa un texto a cifrar',)
 
-st.write('El texto que cifrarás es: ', texto)
+st.write('El texto que cifrarás es: ', cifrado)
